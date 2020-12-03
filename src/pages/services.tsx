8511217b0028398f0ade0d75b68837487b800eae
Benjamin Domey <domeybenjamin1@gmail.com>
2020-12-03 17:29:30 +0000

    added services page, but I'd come back to it later

---
+++ src/pages/services.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import Layout from "../components/organisms/layout";
+import { PageProps } from "gatsby";
+import SliderComponent from "../components/organisms/services/slider";
+
+const ServicePage = ({ location }: PageProps) => {
+  return (
+    <React.Fragment>
+      <Layout location={location}>
+        <React.Fragment>
+          <SliderComponent
+            message={"Our Services"}
+            image={
+              "https://images.unsplash.com/photo-1469406396016-013bfae5d83e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2850&q=80"
+            }
+            // image={
+            //   "https://images.unsplash.com/photo-1518842023089-50e9ac314ad1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1525&q=80"
+            // }
+          />
+          <div className={"my-10"}>
+            <div className="lg:text-center">
+              <h2 className="text-base text-primary-600 font-semibold tracking-wide uppercase">
+                Services
+              </h2>
+              <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                We provide the following services:
+              </p>
+              <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+                Lorem ipsum dolor sit amet consect adipisicing elit. Possimus
+                magnam voluptatum cupiditate veritatis in accusamus quisquam.
+              </p>
+            </div>
+
+            <div id={"child-contact-service"}>will come back here later</div>
+          </div>
+        </React.Fragment>
+      </Layout>
+    </React.Fragment>
+  );
+};
+
+export default ServicePage;
