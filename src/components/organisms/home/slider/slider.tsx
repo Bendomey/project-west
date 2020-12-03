ab780cd8f032288c2635fb080cce75ab712f1cc5
Benjamin Domey <domeybenjamin1@gmail.com>
2020-12-03 16:07:48 +0000

    fixed mobile view issues

--- src/components/organisms/home/slider/slider.tsx
+++ src/components/organisms/home/slider/slider.tsx
@@ -41,19 +41,31 @@ const SliderComponent = () => {
       </div>
       <div
         className={
-          "bg-primary-600 w-full h-full sm:h-full md:h-44 text-gray-200 flex justify-center items-center "
+          "bg-primary-600 w-full h-full sm:h-full md:h-44 py-7 sm:py-7 md:py-0 text-gray-200 flex justify-center items-center "
         }
       >
-        <div className={"w-10/12"}>
-          <p className={"font-light text-center"}>
-            We are an experienced leading Child Contact & Assessment Centre
-            based in London. Our multi-disciplinary team provides services in
-            all areas of Child Contact; Parenting Assessments; Family Support
-            and Mentoring. We ensure the protection and safety of children and
-            young people by providing a forum where parents and children/young
-            people can be together in a safe caring environment with a staff
-            team who work with the family to promote good parenting skills.
-          </p>
+        <div className={"w-11/12 sm:w-11/12 md:w-10/12"}>
+          <Transition
+            show={true}
+            enter="transform transition ease-in-out duration-500 sm:duration-700"
+            enterFrom="-translate-x-full"
+            enterTo="translate-x-0"
+            leave="transform transition ease-in-out duration-500 sm:duration-700"
+            leaveFrom="translate-x-0"
+            leaveTo="-translate-x-full"
+          >
+            <p
+              className={"font-light text-center text-sm sm:text-sm md:text-lg"}
+            >
+              We are an experienced leading Child Contact & Assessment Centre
+              based in London. Our multi-disciplinary team provides services in
+              all areas of Child Contact; Parenting Assessments; Family Support
+              and Mentoring. We ensure the protection and safety of children and
+              young people by providing a forum where parents and children/young
+              people can be together in a safe caring environment with a staff
+              team who work with the family to promote good parenting skills.
+            </p>
+          </Transition>
         </div>
       </div>
     </React.Fragment>
